acb0c67c972b81aaabada82e1a2a35b85a10b880
aalinna <37206656+aalinna@users.noreply.github.com>
2018-04-07 22:31:14 +0800

    Add comments

--- src/main/scala/neuralnetwork/Neuron.scala
+++ src/main/scala/neuralnetwork/Neuron.scala
@@ -4,47 +4,47 @@ import chisel3._
 import chisel3.core.FixedPoint
 import chisel3.util._
 
-class NeuronDataIn extends Bundle {
+class NeuronDataIn extends Bundle {  //数据输入
   val axon = UInt(32.W)
   val weight = Fixed
   val in = Fixed
 }
 
-class NeuronDataOut extends Bundle {
+class NeuronDataOut extends Bundle {   //数据输出
   val out = Fixed
 }
 
-class NeuronIO extends Bundle {
+class NeuronIO extends Bundle {   //输入输出
   val in = Flipped(Decoupled(new NeuronDataIn))
   val out = Decoupled(new NeuronDataOut)
 }
 
-class Neuron(val numAxons: Int) extends Module with CurrentCycle {
+class Neuron(val numAxons: Int) extends Module with CurrentCycle {  //定义神经元
   val io = IO(new NeuronIO)
 
-  val s_idle :: s_busy :: s_done :: Nil = Enum(3)
-  val state = RegInit(s_idle)
+  val s_idle :: s_busy :: s_done :: Nil = Enum(3)  //定义神经元状态：空闲、忙、已完成
+  val state = RegInit(s_idle)  //当前状态sate
 
   val sum = RegInit(0.Fixed)
 
-  switch(state) {
+  switch(state) {   //各状态下任务及状态转换
     is(s_idle) {
       when(io.in.valid) {
-        sum := io.in.bits.weight * io.in.bits.in
-        state := s_busy
+        sum := io.in.bits.weight * io.in.bits.in  //“空闲”时可接受输入并计算
+        state := s_busy     //接受输入后由“空闲”转到“忙”
       }
     }
     is(s_busy) {
       when(io.in.valid) {
-        sum := sum + io.in.bits.weight * io.in.bits.in
+        sum := sum + io.in.bits.weight * io.in.bits.in   //”忙“状态下持续做计算
         when(io.in.bits.axon === (numAxons- 1).U){
-          state := s_done
+          state := s_done   //计算最后一个突触后状态转为“ 完成”
         }
       }
     }
     is(s_done) {
       when(io.out.valid){
-        state := s_idle
+        state := s_idle  //当数据输出之后状态转为空闲
       }
     }
   }

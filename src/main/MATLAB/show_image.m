c7810378b360afab729c97c5b5869ca25800577f
aalinna <37206656+aalinna@users.noreply.github.com>
2018-04-24 12:22:01 +0800

    Fixed paths

--- src/main/MATLAB/show_image.m
+++ src/main/MATLAB/show_image.m
@@ -2,7 +2,7 @@ clear; clc; close all; format compact
 
 disp('Loading data..')
 
-mnist_train_data = csvread('data/mnist_train.csv');
+mnist_train_data = csvread('../../../data/mnist_train.csv');
  
 label = mnist_train_data(2,1);
 img = reshape(mnist_train_data(2,2:end), 28,28)';

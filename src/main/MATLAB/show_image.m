90de0138bb7b30b4610598677f151c7aeb3282e6
aalinna <37206656+aalinna@users.noreply.github.com>
2018-04-24 12:15:31 +0800

    Add data and MATLAB code

---
+++ src/main/MATLAB/show_image.m
@@ -0,0 +1,9 @@
+clear; clc; close all; format compact
+
+disp('Loading data..')
+
+mnist_train_data = csvread('data/mnist_train.csv');
+ 
+label = mnist_train_data(2,1);
+img = reshape(mnist_train_data(2,2:end), 28,28)';
+image(img);
---
+++ src/main/MATLAB/show_sigmoid.m
@@ -0,0 +1,6 @@
+fi(pi)
+
+%%
+x = -10:0.1:10;
+y = sigmoid(x);
+plot(x, y);

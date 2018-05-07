1513c613231bf2c3695a96a8d1e5774152db1199
aalinna <37206656+aalinna@users.noreply.github.com>
2018-05-07 11:04:25 +0800

    Add the rough draft

--- src/main/MATLAB/NeuralNetwork.m
+++ src/main/MATLAB/NeuralNetwork.m
@@ -14,11 +14,11 @@ classdef NeuralNetwork < handle
     end
     
     methods
-        % Initialize the neural network.
+        % Initialize the neural network.初始化神经网络
         function self = NeuralNetwork(num_inputs, num_hidden, num_outputs, learning_rate)
-            self.num_inputs = num_inputs;
-            self.num_hidden = num_hidden;
-            self.num_outputs = num_outputs;
+            self.num_inputs = num_inputs;   %定义输入层数目
+            self.num_hidden = num_hidden;   %定义隐藏层数目
+            self.num_outputs = num_outputs; %定义输出层数目
             
             self.learning_rate = learning_rate;
             
@@ -28,16 +28,16 @@ classdef NeuralNetwork < handle
             self.weights_hidden_outputs = normrnd(0.0, power(self.num_outputs, -0.5), self.num_outputs, self.num_hidden);
         end
         
-        % Train the neural network.
+        % Train the neural network.训练神经网络
         function train(self, inputs, targets)
             inputs = inputs';
             targets = targets';
             
-            hidden_in = self.weights_inputs_hidden * inputs;
-            hidden_out = self.activation_function(hidden_in);
+            hidden_in = self.weights_inputs_hidden * inputs;   %计算输入层输出作为隐藏层的输入
+            hidden_out = self.activation_function(hidden_in);  %计算结果赋给输出
             
-            outputs_in = self.weights_hidden_outputs * hidden_out;
-            outputs_out = self.activation_function(outputs_in);
+            outputs_in = self.weights_hidden_outputs * hidden_out;  %计算隐藏层输出作为输入层的输入
+            outputs_out = self.activation_function(outputs_in);     %计算结果并赋给输出
             
             outputs_errors = targets - outputs_out;
             hidden_errors = self.weights_hidden_outputs' * outputs_errors;
@@ -46,7 +46,7 @@ classdef NeuralNetwork < handle
             self.weights_inputs_hidden = self.weights_inputs_hidden + self.learning_rate .* hidden_errors .* hidden_out .* (1 - hidden_out) * inputs';
         end
         
-        % Test the neural network.
+        % Test the neural network.测试神经网络
         function outputs_out = test(self, inputs)
             inputs = inputs';
             
--- src/main/scala/neuralnetwork/MNIST.scala
+++ src/main/scala/neuralnetwork/MNIST.scala
@@ -1,5 +1,7 @@
 package neuralnetwork
 
-object MNIST {
+import scala.io.Source
 
+object MNIST {
+  val file = Source.fromFile ("C: \\jiangning.txt") //读取文件
 }
